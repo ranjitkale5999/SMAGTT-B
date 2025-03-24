13a538f9a5d99ec113f6e300c1e483e7aeecea7b
Ranjit Gajanan Kale <119128221+Ranjitk0001@users.noreply.github.com>
2025-03-24 22:54:17 +0530

    create spring boot app

---
+++ smagtt/src/main/java/com/gtt/smagtt/SmagttApplication.java
@@ -0,0 +1,14 @@
+package com.gtt.smagtt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmagttApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmagttApplication.class, args);
+		System.out.println("smagtt");
+	}
+
+}
---
+++ smagtt/src/test/java/com/gtt/smagtt/SmagttApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gtt.smagtt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmagttApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
