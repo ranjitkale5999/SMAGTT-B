ddc460609937ac5be2472fa2021e41dbcb13832f
Ranjit Gajanan Kale <119128221+Ranjitk0001@users.noreply.github.com>
2025-05-11 21:58:06 +0530

    ...

--- smagtt/src/main/java/com/gtt/smagtt/login/service/OurUserDetailsService.java
+++ smagtt/src/main/java/com/gtt/smagtt/login/service/OurUserDetailsService.java
@@ -1,10 +1,12 @@
 package com.gtt.smagtt.login.service;
 import com.gtt.smagtt.login.repository.UsersRepo;
+//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+//import org.springframework.stereotype.Service;
 
 @Service
 public class OurUserDetailsService implements UserDetailsService {
