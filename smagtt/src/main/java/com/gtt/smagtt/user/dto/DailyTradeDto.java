02951cdf04079355d844a030924f5bfb27e04f92
Ranjit Gajanan Kale <119128221+Ranjitk0001@users.noreply.github.com>
2025-05-25 13:31:33 +0530

    mapstruct miss field match like name=sharename

--- smagtt/src/main/java/com/gtt/smagtt/user/dto/DailyTradeDto.java
+++ smagtt/src/main/java/com/gtt/smagtt/user/dto/DailyTradeDto.java
@@ -1,18 +1,12 @@
 package com.gtt.smagtt.user.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-//import lombok.Data;
 
 import java.time.LocalDateTime;
 
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class DailyTradeDto {
 
     private long id;
@@ -24,4 +18,6 @@ public class DailyTradeDto {
     private double gannLevel;
     @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime tradeDate;
+
+
 }
--- smagtt/src/main/java/com/gtt/smagtt/user/mapper/DailyTradeMapper.java
+++ smagtt/src/main/java/com/gtt/smagtt/user/mapper/DailyTradeMapper.java
@@ -2,16 +2,12 @@ package com.gtt.smagtt.user.mapper;
 
 import com.gtt.smagtt.user.dto.DailyTradeDto;
 import com.gtt.smagtt.user.entity.DailyTrade;
-
-import org.mapstruct.InheritInverseConfiguration;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.MappingConstants;
-
+import org.mapstruct.*;
 
 import java.util.List;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+
 public interface DailyTradeMapper {
 
     @Mapping(source ="shareName", target = "name")
@@ -24,7 +20,15 @@ public interface DailyTradeMapper {
 
     List<DailyTrade> mapToDailyTradeList(List<DailyTradeDto> dailyTradeDtos);
 }
-
-
-
-
+//@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+//@Mapper(componentModel = "spring")
+//public interface DailyTradeMapper {
+//
+//    DailyTradeDto mapToDailyTradeDto(DailyTrade dailyTrade);
+//
+//    DailyTrade mapToDailyTrade(DailyTradeDto dailyTradeDto);
+//
+//    List<DailyTradeDto> mapToDailyTradeDtoList(List<DailyTrade> dailyTrades);
+//
+//    List<DailyTrade> mapToDailyTradeList(List<DailyTradeDto> dailyTradeDtos);
+//}
